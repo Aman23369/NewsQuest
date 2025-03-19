5b2daa06d542b7fa65b592a86771115ef249783a
Aman Verma <amanverrr123@gmail.com>
2025-03-19 13:30:23 +0530

    fixed error

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import News from './components/News';
-import{
-  BrowserRouter as Router,
- Routes,
- Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
-        <NavBar/>
-        <Routes>
-          <Route exact path="/" element={<News key="home" country="in" category="general"/>}></Route>
-          <Route exact path="/business" element={<News key="business" country="in" category="business"/>}></Route>
-          <Route exact path="/entertainment" element={<News key="entertainment" country="in" category="entertainment"/>}></Route>
-          <Route exact path="/health" element={<News key="health" country="in" category="health"/>}></Route>
-          <Route exact path="/science" element={<News key="science" country="in" category="science"/>}></Route>
-          <Route exact path="/sports" element={<News key="sports" country="in" category="sports"/>}></Route>
-          <Route exact path="/technology" element={<News key="technology" country="in" category="technology"/>}></Route>
-        </Routes>
-        </Router>
-      </div>
-    )
+      <Router basename={process.env.PUBLIC_URL}>
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<News key="home" country="in" category="general" />} />
+            <Route path="/business" element={<News key="business" country="in" category="business" />} />
+            <Route path="/entertainment" element={<News key="entertainment" country="in" category="entertainment" />} />
+            <Route path="/health" element={<News key="health" country="in" category="health" />} />
+            <Route path="/science" element={<News key="science" country="in" category="science" />} />
+            <Route path="/sports" element={<News key="sports" country="in" category="sports" />} />
+            <Route path="/technology" element={<News key="technology" country="in" category="technology" />} />
+          </Routes>
+        </>
+      </Router>
+    );
   }
 }
